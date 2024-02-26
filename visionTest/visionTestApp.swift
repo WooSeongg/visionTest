5952df620f5a21c2cbd89800d1eb1c807f17d6cd
choiws <choiws@smsoftlab.co.kr>
2024-02-26 16:36:29 +0900

    Initial Commit

---
+++ visionTest/visionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionTestApp.swift
+//  visionTest
+//
+//  Created by SMSoftLab on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct visionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
